6e391aa0bacd09f6a0238775dbdc44fbfb38ef1c
Li Ma <li.ma@duke.edu>
2017-01-23 16:45:39 -0500

    Update grove.R

--- R/grove.R
+++ R/grove.R
@@ -1,7 +1,7 @@
 #' grove: A package for functional denoising and functional ANOVA
 #'
 #'The grove package implements a wavelet-domain Bayesian hierarchical model for
-#'function analysis of variance. 
+#'functional analysis of variance. 
 #' 
 #' @docType package
 #' @name grove
